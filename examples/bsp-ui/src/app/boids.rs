23b778b1094d98aa4b1b9ce33ee73a06c8f2ee9d
kang-sw <ki6080@gmail.com>
2024-02-07 23:28:30 +0900

    example.bsp: change defaults

--- examples/bsp-ui/src/app/boids.rs
+++ examples/bsp-ui/src/app/boids.rs
@@ -88,15 +88,15 @@ impl Default for Model {
             bsp: Default::default(),
             rand: fastrand::Rng::with_seed(0),
 
-            tick_delta: 1. / 15.,
+            tick_delta: 1. / 30.,
             enable_tick: true,
 
             area_radius: 160.,
             view_radius: 3.,
             near_radius: 0.5,
-            cohesion_force: 28.5,
-            align_force: 5.,
-            separation_force: 200.,
+            cohesion_force: 24.,
+            align_force: 11.,
+            separation_force: 344.,
             max_speed: 10.,
             predator_avoidance: 150.,
 
