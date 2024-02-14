a6dbcdc006440ec77ef943041bf3315e4994b406
kang-sw <ki6080@gmail.com>
2024-02-15 01:27:18 +0900

    primitive: remove unused type

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ pub mod bitindex;
 pub mod bsp;
 pub mod primitive;
 
-pub use primitive::ControlIntensity;
-
 // Reexport necessary items.
 pub use slotmap::new_key_type as define_key;
 pub use slotmap::Key;
