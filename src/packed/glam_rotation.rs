9ab4145c2ed6ee2a1e6b973beee5b3fe68948a5c
kang-sw <ki6080@gmail.com>
2024-03-17 15:17:14 +0900

    packed: add comment

--- src/packed/glam_rotation.rs
+++ src/packed/glam_rotation.rs
@@ -1,13 +1,15 @@
 use super::*;
 
-/// A packed rotator which eliminated roll component from rotation.
+/// A normalized spherical coordinate to represent a directional vector.
+// TODO: implement vector conversion
 #[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "reflect", derive(Reflect))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(transparent))]
 pub struct PackedDirection<B, const PAN: usize>(B);
 
-/// 패킹된
+/// Packed scaled axis. length is normalized in range `0..PI`
+// TODO: implement quaternion conversion
 #[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "reflect", derive(Reflect))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
